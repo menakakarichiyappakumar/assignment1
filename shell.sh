1b2ca2ba77418ac68cf364faa3e2ed290543be0f
menakakarichiyappakumar <70016091+menakakarichiyappakumar@users.noreply.github.com>
2020-09-21 09:24:37 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,147 +1,144 @@
 user=$(whoami)
 if [ $user = "root" ]; then
-VAR1=$(awk -F "=" '$1=="ID_LIKE" {print$2}' /etc/os-release)
-VAR2='"rhel fedora"'
-VAR3="debian"
+    VAR1=$(awk -F "=" '$1=="ID_LIKE" {print$2}' /etc/os-release)
+    VAR2='"rhel fedora"'
+    VAR3="debian"
+    if [ "$VAR1" = "$VAR2" ]; then
+        echo "RHEL PLATFORM"
+        echo " "
 
-if [ "$VAR1" = "$VAR2" ]; then
-    echo "RHEL PLATFORM"
-    echo " "
-    install(){
-    echo "Installing packages"
-    echo " "
-    yum install httpd mysql-server mysql php php-mysql
+        install(){
+            echo "Installing packages"
+            echo " "
+            yum install httpd mysql-server mysql php php-mysql
+        }
+
+         command(){
+             a=1
+             until [ $a -eq 5 ]
+              do
+                echo "0 - install 1 - start service 2 - stop service 3 - status 4 - version 5 - exit"
+                read a
+                if [ $a == 0 ]; then
+                    echo "Enter the package you want to install "
+                    read pac
+                    yum install $pac
+                elif [ $a == 1 ]; then
+                    echo "Enter the service you want to start"
+                    read ser
+                    if [ -f "/etc/init.d/$ser" ]; then
+                        service $ser start
+                    else
+                        yum install $ser
+                        service $ser start
+                    fi
+                elif [ $a == 2 ]; then
+                    echo "Enter the service you want to stop"
+                    read ser
+                    service $ser stop
+                elif [ $a == 3 ]; then
+                    echo "Enter the service to get status"
+                    read ser
+                    service $ser status
+                elif [ $a == 4 ]; then
+                    echo "Enter the service to get version"
+                    read ser
+                    $ser -v
+                else
+                    echo "bye bye"
+                fi
+              done
+         }
+
+        clone(){
+            service httpd restart
+            service mysqld restart
+            mkdir git
+            echo "Cloning Git"
+            echo " "
+            git clone https://github.com/menakakarichiyappakumar/ass1.git git
+            cp /home/ec2-user/git/info.php /var/www/html/
+            rm -rf git
+        }
+
+        backup(){
+            echo "backing up"
+            echo ""
+            bash backup.sh
+            crontab -l
+        }
+    elif [ "$VAR1" = "$VAR3" ]; then
+        echo "Debian Platform"
+        echo " " 
+
+        install(){
+            echo "Installing packages"
+            apt-get install apache2 php libapache2-mod-php php-mysql mysql-server
+        }
+
+        command(){
+            a=1
+            until [ $a -eq 5 ]
+            do
+                echo "0 - install 1 - start service 2 - stop service 3 - status 4 - version 5 - exit"
+                read a
+                if [ $a == 0 ]; then
+                    echo "Enter the package you want to install "
+                    read pac
+                    apt-get install $pac
+                elif [ $a == 1 ]; then
+                    echo "Enter the service you want to start"
+                    read ser
+                    service="mysqld"
+                    if [ -f "/etc/init.d/$ser" ]; then
+                        service $ser start
+                    else
+                        apt-get install $ser
+                        service $ser start
+                    fi
+                elif [ $a == 2 ]; then
+                    echo "Enter the service you want to stop"
+                    read ser
+                    service $ser stop
+                elif [ $a == 3 ]; then
+                    echo "Enter the service to get status"
+                    read ser
+                    service $ser status
+                elif [ $a == 4 ]; then
+                    echo "Enter the service to get version"
+                    read ser
+                    $ser -v
+                else
+                    echo "bye bye"
+                fi
+            done
+         }
+
+         clone(){
+            service apache2 start
+            service mysqld start
+            mkdir git
+            git clone https://github.com/menakakarichiyappakumar/ass1.git git
+            cp /home/ubuntu/git/info.php /var/www/html/
+            rm -rf git
+        }
+
+        backup(){
+            echo "backing up"
+            echo " "
+            bash backup.sh
+            crontab -l
+        }
+    else
+        echo "No match"
+    fi
+
+    lamp(){
+        install
+        command
+        clone
+        backup
     }
-
-    command(){
-    a=1
-    until [ $a -eq 5 ]
-    do
-    echo "0 - install 1 - start service 2 - stop service 3 - status 4 - version 5 - exit"
-    read a
-    if [ $a == 0 ]; then
-      echo "Enter the package you want to install "
-      read pac
-      yum install $pac
-    elif [ $a == 1 ]; then
-      echo "Enter the service you want to start"
-      read ser
-      service="mysqld"
-      if [ -f "/etc/init.d/$ser" ]; then
-       service $ser start
-      else
-       yum install $ser
-       service $ser start
-      fi
-    elif [ $a == 2 ]; then
-      echo "Enter the service you want to stop"
-      read ser
-      service $ser stop
-    elif [ $a == 3 ]; then
-      echo "Enter the service to get status"
-      read ser
-      service $ser status
-    elif [ $a == 4 ]; then
-      echo "Enter the service to get version"
-      read ser
-      $ser -v
-    else
-      echo "bye bye"
-    fi
-    done
-   }
-  clone(){
-  service httpd restart
-  service mysqld restart
-  mkdir git
-  echo "Cloning Git"
-  echo " "
-  git clone https://github.com/menakakarichiyappakumar/ass1.git git
-  cp /home/ec2-user/git/info.php /var/www/html/
-  rm -rf git
-  }
-  
-  backup(){
-  echo "backing up"
-  echo ""
-  bash backup.sh
-  crontab -l
-  }
-
-  elif [ "$VAR1" = "$VAR3" ]; then
-    echo "Debian Platform"
-    echo " " 
-
-    install(){
-    echo "Installing packages"
-    apt-get install apache2 php libapache2-mod-php php-mysql mysql-server
-     }
-   
-   command(){
-    a=1
-    until [ $a -eq 5 ]
-    do
-    echo "0 - install 1 - start service 2 - stop service 3 - status 4 - version 5 - exit"
-    read a
-    if [ $a == 0 ]; then
-      echo "Enter the package you want to install "
-      read pac
-      apt-get install $pac
-    elif [ $a == 1 ]; then
-      echo "Enter the service you want to start"
-      read ser
-      service="mysqld"
-      if [ -f "/etc/init.d/$ser" ]; then
-       service $ser start
-      else
-       apt-get install $ser
-       service $ser start
-      fi
-    elif [ $a == 2 ]; then
-      echo "Enter the service you want to stop"
-      read ser
-      service $ser stop
-   elif [ $a == 3 ]; then
-      echo "Enter the service to get status"
-      read ser
-      service $ser status
-    elif [ $a == 4 ]; then
-      echo "Enter the service to get version"
-      read ser
-      $ser -v
-    else
-      echo "bye bye"
-    fi
-    done
-   }
-
-   clone(){
-   service apache2 start
-   service mysqld start
-   mkdir git
-   git clone https://github.com/menakakarichiyappakumar/ass1.git git
-   cp /home/ubuntu/git/info.php /var/www/html/
-   rm -rf git
-   }
- 
-   backup(){
-   echo "backing up"
-   echo " "
-   bash backup.sh
-   crontab -l
-   }
-  else
-    echo "No match"
-  fi
-
-lamp(){
-install
-command
-clone
-backup
-}
-"$@"
-
 else
-echo "only root user is allowed"
+    echo "only root user is allowed"
 fi
